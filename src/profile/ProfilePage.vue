3788c28cd03759deaa7b358fb65312cf05c5b132
RomeoN451 <74374030+RomeoN451@users.noreply.github.com>
2021-01-20 17:58:12 +0200

    Update ProfilePage.vue
    Update profile page

--- src/profile/ProfilePage.vue
+++ src/profile/ProfilePage.vue
@@ -9,8 +9,11 @@
                 <icon class="icon-dumbbell mr-3" icon="dumbbell" />
                 {{ account.user.first_name + ' ' + account.user.last_name}}
             </h2>
-            <div class="row h5 mt-3" >{{account.user.email}}</div>
-            <div class="row h5 mt-3" >{{account.user.age}} years old</div>
+            <div class="row h5 mt-3">{{account.user.email}}</div>
+            <input type="text" v-model="first_name" name="first_name" placeholder="Change your first name"> </input>
+            <input type="text" v-model="last_name" name="last_name" placeholder="Change your last name"> </input>
+            <div class="row h5 mt-3">{{account.user.age}} years old</div>
+            <input type="text" v-model="age" name="age" placeholder="Change your age"> </input>
             <div class="row h5 mt-3">
                 <div v-if="account.user.gender == 'M'">Male</div>
                 <div v-if="account.user.gender == 'F'">Female</div>
@@ -19,16 +22,20 @@
             <div class="row h5 mt-3">
                 <div>Height: {{ account.user.height }} cm </div>
             </div>
+            <input type="text" v-model="height" name="height" placeholder="Change your height"> </input>
             <div class="row h5 mt-3">
                 <div>Weight: {{ account.user.weight }} kg </div>
             </div>
+            <input type="text" v-model="weight" name="weight" placeholder="Change your weight"> </input>
             <div class="row h5 mt-3">
-                <div>Purpose:  </div>
+                <div>Purpose:  </div>   
                 <div v-if="account.user.purpose == 'L'">Lose weight</div>
                 <div v-if="account.user.purpose == 'M'">Muscle increase</div>
                 <div v-if="account.user.purpose == 'S'">Stay in shape</div>
             </div>
+            <input type="text" v-model="purpose" name="purpose" placeholder="Change your purpose"> </input>
         </div>
+        <button class="btn btn-primary" >Submit</button>
     </div>
 </template>
 <script>
